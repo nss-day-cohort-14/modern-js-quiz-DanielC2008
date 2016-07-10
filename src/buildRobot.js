38b3a5620a210e2deda349281e057f3dc708079b
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-07-10 10:37:18 -0500

    drying up build robot function

--- src/buildRobot.js
+++ src/buildRobot.js
@@ -7,40 +7,54 @@ let nameTwo;
 //STORE NAME
 $("input").keypress( (e) => {
 	 let enter = e.which === 13 ?  
-		(e.target.id === "player1" ? (nameOne = $(e.target).val()) : (nameTwo = $(e.target).val()))
-	 : null;
+		(e.target.id === "player1" ? (nameOne = $(e.target).val()) : (nameTwo = $(e.target).val())) 
+		: null;
 });
 
 
 //CREATE ROBOT OBJECT
 $("select").change( (e) => {
 	let robot;
+	let divId = e.target.id;
 	typesArray.forEach( (index) => {
 		let getModel = index.model === $(e.target).val() ? (robot = index) : null; 
 	});
-		let getRobotDiv = e.target.id === "robotSelect1" ? createRbt1(robot) : createRbt2(robot);
+		createRbt(robot, divId);
 });
 
 
 //ADD NAME AND SEND TO DOM
-let createRbt1 = (robot) => {
-	let a = `Model: ${robot.model}`, b = `Name: ${nameOne}`, c = `Current Health: ${robot.health}`, d = `Damage: ${robot.attack}`;
-	let robAttr = [a, b, c, d];
-	let rob = $("#robot1");
-	rob.empty();
-	robAttr.forEach( (index) => {
+let createRbt = (robot, divId) => {
+	let robotEl; 
+	if (divId === "robotSelect1") {
+		 robotEl = $("#robot1");
+		if (nameOne !== undefined) {
+		 robot.name = nameOne;
+		} 
+	}	
+	else {
+		 robotEl = $("#robot2");
+		if (nameTwo !== undefined) {
+		 robot.name = nameTwo;
+		} 
+	}	
+
+		let a = `Model: ${robot.model}`, b = `Name: ${robot.name}`, c = `Current Health: ${robot.health}`, d = `Damage: ${robot.attack}`;
+		let robAttr = [a, b, c, d];
+		robotEl.empty();
+		robAttr.forEach( (index) => {
 		let create = $("<div>").html(`${index}`);
-		rob.append(create);
+		robotEl.append(create);
 	});
 };
 
-let createRbt2 = (robot) => {
-	let a = `Model: ${robot.model}`, b = `Name: ${nameTwo}`, c = `Current Health: ${robot.health}`, d = `Damage: ${robot.attack}`;
-	let robAttr = [a, b, c, d];
-	let rob = $("#robot2");
-	rob.empty();
-	robAttr.forEach( (index) => {
-		let create = $("<div>").html(`${index}`);
-		rob.append(create);
-	});
-};
+// let createRbt2 = (robot) => {
+// 	let a = `Model: ${robot.model}`, b = `Name: ${nameTwo}`, c = `Current Health: ${robot.health}`, d = `Damage: ${robot.attack}`;
+// 	let robAttr = [a, b, c, d];
+// 	let rob = $("#robot2");
+// 	rob.empty();
+// 	robAttr.forEach( (index) => {
+// 		let create = $("<div>").html(`${index}`);
+// 		rob.append(create);
+// 	});
+// };
